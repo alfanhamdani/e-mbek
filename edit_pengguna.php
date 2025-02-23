a7fdcef78004f597f4d760e4eaa4b4bfe00c72e1
alfanhamdani <149344830+alfanhamdani@users.noreply.github.com>
2025-02-23 20:27:23 +0700

    Menambahkan menu pindai kode di sidebar

--- edit_pengguna.php
+++ edit_pengguna.php
@@ -184,6 +184,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <a href="daftar_pakan.php" class="w3-bar-item w3-button w3-border">Daftar Pakan</a>
         <a href="daftar_perawatan.php" class="w3-bar-item w3-button w3-border">Daftar Perawatan</a>
         <a href="hasil_labarugi.php" class="w3-bar-item w3-button w3-border">Hasil Laba Rugi</a>
+        <a href="scan_code.php" class="w3-bar-item w3-button w3-border">Pindai Kode</a>
         <?php if ($username === 'admin') { ?>
             <a href="daftar_pengguna.php" class="w3-bar-item w3-button w3-border">Daftar Pengguna</a>
         <?php } ?>
